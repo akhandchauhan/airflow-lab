99c1535b5ddc860ee7ff6471849c16b047f0bef7
Panda Singh <112802105+akhandchauhan@users.noreply.github.com>
2026-08-30 07:01:49 +0000

    course: 01 diamond complete

--- dags/task_1_foundations.py
+++ dags/task_1_foundations.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 import pendulum
 from airflow.sdk import task, dag
 
@@ -11,8 +12,8 @@ from airflow.sdk import task, dag
     default_args={"owner": "panda singh", "retries": 2},
 )
 def pipeline():
-    @task
-    def fetch_orders(multiple_outputs=True):
+    @task(multiple_outputs=True)
+    def fetch_orders():
         return {"gross_revenue": 2500, "order_count": 20, "currency": "USD"}
 
     @task
@@ -23,9 +24,21 @@ def pipeline():
     def net_revenue(gross_rev: int, refunds: float):
         return float(gross_rev - refunds)
 
+    @task
+    def avg_order_value(net_rev: float, order_cnt: int) -> float:
+        return round(net_rev/order_cnt, 2)
+
+    @task
+    def report(net_rev: float, avg_order_val: float, currency: str) -> None:
+        print(
+            f"net revenue = {net_rev}{currency}, acrosss 300 orders, aov ={avg_order_val}")
+        return None
+
     order_info = fetch_orders()
     refund_cnt = fetch_refunds()
-    net_revenue(order_info['gross_revenue'], refund_cnt)
+    n_revenue = net_revenue(order_info['gross_revenue'], refund_cnt)
+    aov = avg_order_value(n_revenue, order_info['order_count'])
+    report(n_revenue, aov, order_info['currency'])
 
 
 pipeline()
