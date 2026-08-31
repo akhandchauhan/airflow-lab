7d8e10ad2164258b50cceead6deb1df4fbca0b90
Panda Singh <112802105+akhandchauhan@users.noreply.github.com>
2026-08-31 10:10:10 +0000

    did session 2

---
+++ dags/task-2/task_2.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import pendulum
+from airflow.sdk import DAG, chain
+from airflow.providers.standard.operators.empty import EmptyOperator
+
+
+with DAG(
+    dag_id="02_classic_operators",
+    start_date=pendulum.datetime(2026, 1, 1, tz="UTC"),
+    schedule=None,
+    catchup=False,
+    tags=["course", "demo"],
+    default_args={"owner": "akhand", "retries": 2},
+) as dag:
+
+    extract = EmptyOperator(task_id="extract")
+    validate_schema = EmptyOperator(task_id="validate_schema")
+    validate_nulls = EmptyOperator(task_id="validate_nulls")
+    validate_ranges = EmptyOperator(task_id="validate_ranges")
+    load = EmptyOperator(task_id="load")
+    notify = EmptyOperator(task_id="notify")
+
+    # extract >> [validate_schema, validate_nulls,
+    #             validate_ranges] >> load >> notify
+
+    chain(extract, [validate_schema, validate_nulls,
+          validate_ranges], load, notify)
