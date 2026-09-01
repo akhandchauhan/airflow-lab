e96f954c37d8b1087bb6c376f95a3696393131dd
Panda Singh <112802105+akhandchauhan@users.noreply.github.com>
2026-09-01 17:21:24 +0000

    did task 3

--- dags/task-3/03_task_groups.py
+++ dags/task-3/03_task_groups.py
@@ -9,6 +9,41 @@ from airflow.sdk import dag, task_group, task
     start_date=pendulum.datetime(2026, 9, 1, tz='UTC'),
     catchup=False,
     schedule=None,
-    args=['task_group', 'override'],
-    tags={"owner": "akhand", "retries": 2},
+    tags=['task_group', 'override'],
+    default_args={"owner": "akhand", "retries": 2},
 )
+
+def pipeline():
+
+    @task_group(group_id='src')
+    def load_source(name: str) -> None:
+
+        @task
+        def download(src: str) -> str:
+            return f"gs://{src}"
+
+        @task_group(group_id='quality')
+        def run_checks(path: str) -> str:
+
+            @task
+            def check_nulls(path: str) -> str:
+                return f"No nulls found in this {path} path"
+
+            @task
+            def check_schema(checked:str) -> str:
+                return "everything okay"
+
+            return check_schema(check_nulls(path))
+
+        @task
+        def stage(path: str) -> None:
+            print(f"staged {path}")
+
+        stage(run_checks(download(name)))
+
+
+    for name in ['orders', 'users', 'products']:
+        load_source.override(group_id = f"src_{name}")(name)
+
+
+pipeline()
