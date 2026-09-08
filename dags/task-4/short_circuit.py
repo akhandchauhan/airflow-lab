c702e4791dc019c8d383e35e6bc2931717a69252
Panda Singh <112802105+akhandchauhan@users.noreply.github.com>
2026-09-08 04:59:16 +0000

    solved session 4 short circuite example

---
+++ dags/task-4/short_circuit.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+import pendulum
+from airflow.sdk import dag, task
+
+
+@dag(
+    dag_id="s04_short_circuit_example_exercise",
+    start_date=pendulum.datetime(2026, 1, 1, tz="UTC"),
+    schedule=None,
+    catchup=False,
+    tags=["session-04", "branching"],
+    default_args={"owner": "akhand", "retries": 1},
+)
+def pipeline():
+    @task.short_circuit                # ← THE MECHANIC
+    def has_new_data() -> bool:
+        new_rows = 5
+        return new_rows > 0            # return False → EVERYTHING downstream is skipped
+
+    @task
+    def load_data():
+        print("loading data")
+
+    has_new_data() >> load_data()
+
+
+pipeline()
---
+++ dags/task-4/short_circuit_2.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+import pendulum
+from airflow.sdk import dag, task
+
+
+@dag(
+    dag_id='session_04_short_circuit_eg_stack_overflow',
+    catchup=False,
+    schedule=None,
+    start_date=pendulum.datetime(2026, 9, 8, tz='UTC'),
+    default_args={'retries': 2, 'owner': 'Panda-Singh'},
+    tags=['session-4', 'short_circuit'],
+)
+def pipeline():
+    @task()
+    def cnt_unanswered_q() -> int:
+        return 200_000
+
+    @task.short_circuit
+    def check_unanswered_cnt(cnt: int):
+        return True if cnt > 0 else False
+
+    @task()
+    def build_report():
+        print("building the product-health report")
+
+    task1 = check_unanswered_cnt(cnt_unanswered_q())
+    task1 >> build_report()
+
+
+pipeline()
